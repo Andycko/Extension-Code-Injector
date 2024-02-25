cbc07173d146fd682097776d351a28d0461f9b7f
Andrej Szalma <andy.szalma@gmail.com>
2024-02-25 20:25:50 +0000

    fix: style

--- web/src/App.js
+++ web/src/App.js
@@ -14,7 +14,7 @@ function QueryClientProviderWrapper() {
     return (
         <QueryClientProvider client={queryClient}>
             <NextUIProvider>
-                <main className="dark bg-background text-foreground h-screen w-screen flex items-center justify-center">
+                <main className="dark bg-background text-foreground min-h-screen min-w-screen flex items-center justify-center">
                     <App />
                 </main>
             </NextUIProvider>
@@ -24,7 +24,7 @@ function QueryClientProviderWrapper() {
 
 function App() {
     return (
-        <div className="main-wrapper p-6">
+        <div className="main-wrapper w-full xl:w-auto p-6">
             <h1 className="font-black text-4xl">Extension BotNet</h1>
             <h2 className="font-bold text-2xl text-success mb-10">$> Remote Command Execution</h2>
             <section className="body-wrapper flex xl:gap-3 gap-6 flex-wrap justify-center xl:justify-start">
