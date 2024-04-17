c7a23aba823f9c426b2cfe624e1ad1674045926c
Andrej Szalma <andy.szalma@gmail.com>
2024-04-17 12:24:17 +0100

    feat: add ECS, ECR, LB

--- infra/main.tf
+++ infra/main.tf
@@ -10,9 +10,10 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region = var.aws_region
 }
 
+# S3
 resource "aws_s3_bucket" "data_blob_storage" {
   bucket = "data-blob-storage"
 
@@ -48,3 +49,79 @@ data "aws_iam_policy_document" "allow_access_from_another_account" {
     ]
   }
 }
+
+# ECR
+resource "aws_ecr_repository" "app_repository" {
+  name = "my-nodejs-app"
+}
+
+# ECS
+resource "aws_ecs_cluster" "app_cluster" {
+  name = "my-ecs-cluster"
+}
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = var.app_name
+  container_definitions    = jsonencode([
+    {
+      name  = var.app_name
+      image = "${aws_ecr_repository.app_repository.repository_url}:latest"
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 0
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "app_service" {
+  depends_on = [aws_ecs_task_definition.app_task]
+
+  name            = "${var.app_name}-service"
+  cluster         = aws_ecs_cluster.app_cluster.id
+  task_definition = aws_ecs_task_definition.app_task.arn
+  desired_count   = 1
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app_target_group.arn
+    container_name   = var.app_name
+    container_port   = 3000
+  }
+
+  network_configuration {
+    subnets          = ["subnet-abc123", "subnet-def456"]  # Specify your subnets
+    security_groups  = ["sg-123456"]                       # Specify your security groups
+    assign_public_ip = true
+  }
+}
+
+# Load Balancer
+resource "aws_lb" "app_lb" {
+  name               = "${var.app_name}-lb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = ["subnet-abc123", "subnet-def456"]
+  security_groups    = ["sg-123456"]
+}
+
+resource "aws_lb_target_group" "app_target_group" {
+  name     = "${var.app_name}-tg"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = "vpc-123456"
+}
+
+resource "aws_lb_listener" "app_lb_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_target_group.arn
+  }
+}
+
+
---
+++ infra/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+
+variable "app_name" {
+    description = "Application name"
+    default     = "extension-botnet"
+}
